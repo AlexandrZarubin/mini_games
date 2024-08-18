d4c4bc0d9fa3c2007001dcd6182aef5452a240d7
Alexander Zarubin <whitesashka@gmail.com>
2024-08-19 02:43:03 +0500

    002_console_arkanoid add comment

--- 002_console_arkanoid/main.cpp
+++ 002_console_arkanoid/main.cpp
@@ -8,11 +8,13 @@
 using namespace std;
 
 // Функция для инициализации строки
+// Заполняем строку символом '#'
 void init(string&str,const int width)
 {
 
 	str = string(width, '#');
 }
+
 // Функция для копирования по вектору и инициализации каждой строки
 void initializeVector(vector<string>& vec, const int width) 
 {
@@ -21,6 +23,9 @@ void initializeVector(vector<string>& vec, const int width)
 		init(str, width); // Инициализируем каждую строку вектора
 	}
 }
+
+// Функция для вставки символов в строки вектора
+// Заполняем строки символами: границы символом Symbol, внутренние символом Symbol2
 void InsertSymbol(vector<string>& vec,const int width,const string Symbol = " ", const char Symbol2 = ' ')
 {
 	for (int i = 1; i < vec.size(); ++i)
@@ -28,10 +33,14 @@ void InsertSymbol(vector<string>& vec,const int width,const string Symbol = " ",
 		vec[i]= Symbol + string(width-2, Symbol2) + Symbol;
 	}
 }
+
+// Функция для вывода строки на экран
 void Print(const string& str)
 {
 	cout << str;
 }
+
+// Функция для вывода всех строк вектора на экран
 void PrintAll(const vector<string>& vec)
 {
 	//for (const string& str : vec)
@@ -39,37 +48,48 @@ void PrintAll(const vector<string>& vec)
 	{
 		Print(vec[i]);
 		
-		if (i<vec.size()-1)
+		if (i<vec.size()-1)// Печатаем перевод строки после каждой строки, кроме последней
 		{
 			cout << endl;
 		}
 	}
 }
+
+// Структура для представления ракетки
 struct Tracket
 {
-	int x;
-	int y;
-	int w;
+	int x;// Позиция X
+	int y;// Позиция Y
+	int w;// Ширина ракетки
 };
+
+// Функция для инициализации ракетки
 void inittracket(Tracket& tr,const int width,const int height)
 {
-	tr.w = 7;
-	tr.x=(width - tr.w) / 2;
-	tr.y = height - 1;
+	tr.w = 7;				// Устанавливаем ширину ракетки
+	tr.x=(width - tr.w) / 2;// Центрируем ракетку по X
+	tr.y = height - 1;		// Устанавливаем ракетку в нижнюю часть экрана
 }
+
+// Функция для размещения ракетки на экране
 void PutRacket(vector<string>& vec,Tracket& tr)
 {
 	for (int i = tr.x; i < tr.x+tr.w; i++)
 	{
-		vec[tr.y][i] = '@';
+		vec[tr.y][i] = '@';// Размещаем символ '@' на позиции ракетки
 	}
 }
+
+// Функция для перемещения ракетки
 void moveRacket(Tracket& tr,int x, const int width)
 {
 	tr.x = x;
+	// Проверка, чтобы ракетка не вышла за границы экрана
 	if (tr.x < 1)tr.x=1;
 	if (tr.x + tr.w >= width)tr.x = width - 1 - tr.w;
 }
+
+// Функция для установки позиции курсора в консоли
 void setcur(int x, int y)
 {
 	COORD coord;
@@ -79,26 +99,28 @@ void setcur(int x, int y)
 }
 int main()
 {
-	srand(static_cast<unsigned>(time(0)));
-	const int width = 65;
-	const int height = 25;
-	char c;
-	vector<string> mas(height, string(width, ' '));
-	Tracket trackt;
-	inittracket(trackt, width, height);
-	init(mas[0], width);
-	initializeVector(mas, width);
+	srand(static_cast<unsigned>(time(0)));							// Заполнение границ
+	const int width = 65;											// Ширина экрана
+	const int height = 25;											// Высота экрана
+	vector<string> mas(height, string(width, ' '));					// Инициализация вектора строк
+	Tracket trackt;													// Создание структуры для ракетки
+	inittracket(trackt, width, height);								// Инициализация ракетки
+	init(mas[0], width);											// Инициализация первой строки (границы)
+	initializeVector(mas, width);									// Инициализация всех строк
 	do
 	{
+		// Очистка экрана
 		//system("cls");
-		setcur(0, 0);
-		InsertSymbol(mas, width, "#", ' ');
-		PutRacket(mas, trackt);
-		PrintAll(mas);
+		setcur(0, 0);												// Установка курсора в начало
+		InsertSymbol(mas, width, "#", ' ');							// Заполнение границ
+		PutRacket(mas, trackt);										// Размещение ракетки
+		PrintAll(mas);												// Вывод на экран
+
+		// Управление ракеткой с использованием состояния клавиш
 		//c = _getch();
 		//if (c == 'a')moveRacket(trackt, trackt.x - 1, width);
-		if (GetKeyState('A')>=0)moveRacket(trackt, trackt.x + 1, width);
-		if (GetKeyState('D')>=0)moveRacket(trackt, trackt.x - 1, width);
+		if (GetKeyState('A')>=0)moveRacket(trackt, trackt.x + 1, width);// Перемещение влево
+		if (GetKeyState('D')>=0)moveRacket(trackt, trackt.x - 1, width);// Перемещение вправо
 		//if (c == 'd')moveRacket(trackt, trackt.x + 1, width);
 		
 		Sleep(10);
